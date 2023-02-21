1c39c246c986e17dbb32e57b88403782781a161a
Luis Angel Inga Mendoza <lingamen@emeal.nttdata.com>
2023-02-21 16:50:19 -0500

    Initial Commit

---
+++ CombineSwiftUI/CombineSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineSwiftUIApp.swift
+//  CombineSwiftUI
+//
+//  Created by Luis Angel Inga Mendoza on 21/02/23.
+//
+
+import SwiftUI
+
+@main
+struct CombineSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
